7d14f0c57d7bed8097a914d4bcf44638185d3b62
grzegorz rzopedzik <rozpedzik@gmail.com>
2024-03-18 22:59:40 +0100

    test

--- src/app.js
+++ src/app.js
@@ -6,9 +6,13 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.get("api", (req, res) => {
+app.get("/api", (req, res) => {
   res.json({ message: "Hello World from API" });
 });
+
+app.get("/api/test1", (req, res) => {
+    res.json({ message: "Hello World from API test1" });
+  });
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
